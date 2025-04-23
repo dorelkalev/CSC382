1dc9eb15b9ba178e3f542eda08d12d2b2435c269
Dorel Kalev <158530164+dorelkalev@users.noreply.github.com>
2025-04-23 18:43:32 -0400

    Update Project-4.cpp

--- Project-4.cpp
+++ Project-4.cpp
@@ -61,30 +61,13 @@ MinHeap<T>& MinHeap<T>::Delete(T& x) {
     return *this;
 }
 
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-
 void heapSort(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for (int i = n - 1; i >= 0; i--) {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
+    MinHeap<int> heap(n);
+    for (int i = 0; i < n; ++i) {
+        heap.Insert(arr[i]);
+    }
+    for (int i = 0; i < n; ++i) {
+        heap.Delete(arr[i]);
     }
 }
 
